e258569326ac76696b1052829dbcd2776d528761
amyhenning <henning.amy@gmail.com>
2018-06-01 03:08:26 +0000

    Updated find_missing_number.rb to pass Codewars tests

--- find_missing_number.rb
+++ find_missing_number.rb
@@ -12,12 +12,17 @@ def find_missing_number(sequence)
     end
   end
   
-  numbers = strings.map { |s| s.to_i }
+  numbers = strings.map { |s| s.to_i }.sort!
   if numbers.include?(1) == false
     return 1
   elsif numbers.each_cons(2).all? { |x, y| y == x + 1 } == true
     return 0
   else
-    return (numbers.min .. numbers.max).reduce(0, &:+) - numbers.reduce(0, &:+)
+    # return (numbers.min .. numbers.max).reduce(0, &:+) - numbers.reduce(0, &:+)
+    numbers.each_with_index do |num, i|
+      if num != i + 1
+        return i + 1
+      end
+    end
   end
 end
